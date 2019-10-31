454f7f904b960967551cf59e5984677afe472324
FRANKLIN SAMBONI CASTILLO <frasam@eltiempo.com>
2019-10-31 11:24:52 -0500

    Example Project

--- Xcode11/ViewController.swift
+++ Xcode11/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var hideStatusBar = true
+    
+    override var prefersStatusBarHidden: Bool {
+        return hideStatusBar
+    }
+    
+    override var preferredStatusBarUpdateAnimation: UIStatusBarAnimation{
+        return .fade
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        hideStatusBar = true
+        self.navigationController?.navigationBar.isHidden = true
+        self.setNeedsStatusBarAppearanceUpdate()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        hideStatusBar = false
+        self.navigationController?.navigationBar.isHidden = false
+        self.setNeedsStatusBarAppearanceUpdate()
+    }
 
+}
+
+extension UINavigationController {
+    open override var prefersStatusBarHidden: Bool{
+        return topViewController?.prefersStatusBarHidden ?? false
+    }
 }
 
